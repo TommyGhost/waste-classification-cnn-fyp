ab8a676ca1dd9e487b6af37d8fdb51cbf450263b
TommyGhost <hanimashauntomisin@gmail.com>
2024-08-05 19:58:33 +0100

    first commit

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MyCNNModel(nn.Module):
+    def __init__(self, pretrained=False):
+        super(MyCNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(64 * 54 * 54, 256)
+        self.fc2 = nn.Linear(256, 8)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 54 * 54)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
---
+++ model_ui.py
@@ -0,0 +1,36 @@
+# Import necessary libraries
+import streamlit as st
+from fastai.vision.all import *
+from PIL import Image
+import numpy as np
+
+# Load your FastAI model
+learn = load_learner('export10.pkl')
+
+# Create a Streamlit app
+st.title('Waste Classification App')
+
+# Add a file uploader
+uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'png', 'jpeg', 'jfif', 'webp'])
+
+# Preprocess the uploaded image
+if uploaded_file is not None:
+    image = load_image(uploaded_file)
+
+# Make predictions and display results
+if st.button('Predict'):
+    st.write('Predicting...')
+    pred, pred_idx, probs = learn.predict(image)
+    disposal_type = {
+        'battery': 'Special Handling',
+        'biological': 'Compostable',
+        'cardboard': 'Recyclable',
+        'glass': 'Recyclable',
+        'metal': 'Recyclable',
+        'paper': 'Recyclable',
+        'plastic': 'Recyclable/Landfill',
+        'trash': 'Landfill'
+    }
+    st.write('Predicted Waste Class:', pred.capitalize())
+    st.write('Confidence:', f'{probs[pred_idx].item() * 100:.2f}%')
+    st.write('Disposal Instruction:', disposal_type[pred])
